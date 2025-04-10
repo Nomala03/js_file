33a9a2f602ad21edca7b0b60ee409bb267be1641
Nomala03 <mbalimadonsela03@gmail.com>
2025-04-10 22:03:02 +0200

    checking if repo working

---
+++ variables.js
@@ -0,0 +1,11 @@
+// Exercise: 1
+
+var name = "Mbali";
+var age = 26;
+var isStudent = true;
+
+//print the three variables
+console.log("name", Name );
+console.log("age", age );
+console.log("isStudent", isStudent );
+
